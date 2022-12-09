becbf68a347ca3aac77c62db220ed2d31420f67d
Tania <118524277+TaniaSoleraLucena@users.noreply.github.com>
2022-12-09 09:44:27 +0100

    Add files via upload

---
+++ Tomorrowland/JS/tomorowland.js
@@ -0,0 +1,18 @@
+/***********************************************************/
+/* Web de Tomorrowland                                    */
+/* JS para index                                           */
+/* autora: Tania Solera Lucena                             */
+/* versión: 1.0                                            */
+/* 8 de DIciembre de 2022                                  */
+/***********************************************************/
+
+/* JS QUE REGULA LA BARRA DE NAVEGACION                    */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
